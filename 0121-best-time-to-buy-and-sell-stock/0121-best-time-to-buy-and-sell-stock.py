dd1b9dfae47f1fc60539ebf7990e0fc611e0117b
Maria Bwalya <143863102+MariaBwalya@users.noreply.github.com>
2026-02-25 22:35:17 -0600

    Time: 77 ms (19.8%), Space: 28.6 MB (81.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l,r = 0,1 # left -buying, right- selling
+
+        l,r = 0, 1
         maxP = 0
 
         while r < len(prices):
@@ -11,3 +12,4 @@ class Solution:
                 l=r
             r+=1
         return maxP            
+
